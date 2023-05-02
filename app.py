96db9e0870aa5bb7133c2b121a068956bf9cc309
laurabejder <laurabejder@gmail.com>
2023-05-01 21:09:17 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,43 @@
 # server.py
-from flask import Flask, render_template
-
-from datetime import datetime
+from flask import Flask
+from flask import render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.sqlite3'
+db = SQLAlchemy(app)
+
+with app.app_context():
+    db.Model.metadata.reflect(db.engine)
+
+class Cases_en(db.Model):
+    __tablename__ = 'en_data'
+    __table_args__ = {'extend_existing': True }
+    court_file_number = db.Column(db.Text, primary_key=True)   
+
 @app.route("/")
 def index():
-    return render_template("index.html", now = datetime.now())
+    cases_count = Cases_en.query.count()
+
+    courts = Cases_en.query.with_entities(Cases_en.court_en).distinct().all()
+    courts = [court[0] for court in courts]
+    return render_template("index.html", count=cases_count, courts=courts)
+
+
+
+@app.route('/court/cases/<slug>')
+def detail(slug):
+    case = Cases_en.query.filter_by(slug=slug).first()
+    return render_template("detail.html", case=case)
+
+
+
+@app.route('/court/<court_name>')
+def court(court_name):
+    court_meetings = Cases_en.query.filter_by(court_en=court_name).all()
+    return render_template("court.html", courts=court_name, count=len(court_meetings), location=court_name, meetings=court_meetings)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
